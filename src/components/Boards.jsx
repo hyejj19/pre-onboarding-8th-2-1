bcdae4651558da2440f30d150617917e7318fa0a
Hyejung Park <hyejj19@naver.com>
2023-01-04 17:17:13 +0900

    ✨ Feat :  Boards 기본 구조 퍼블리싱

--- src/components/Boards.jsx
+++ src/components/Boards.jsx
@@ -1,10 +1,20 @@
 import styled from 'styled-components';
 
+import Button from './Button';
+import Cards from './Cards';
+
 const Boards = () => {
   return (
     <BoardsContainer>
-      <MenuContainer />
-      <CardsContainer />
+      <MenuContainer>
+        <Subject>Subject</Subject>
+        <Button />
+      </MenuContainer>
+      <Board>
+        <Cards />
+        <Cards />
+        <Cards />
+      </Board>
     </BoardsContainer>
   );
 };
@@ -22,12 +32,31 @@ const MenuContainer = styled.div`
   width: 100%;
   height: 50px;
   min-height: 50px;
-  background-color: green;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 1rem;
 `;
 
-const CardsContainer = styled.div`
+const Subject = styled.h2`
+  font-weight: 500;
+  font-size: 1.2rem;
+`;
+
+const Board = styled.div`
   width: 100%;
   height: 100%;
   min-height: 300px;
   background-color: orange;
+  display: flex;
+  padding: 1rem;
+
+  @media screen and (max-width: 500px) {
+    flex-direction: column;
+    justify-content: center;
+  }
+  @media screen and (min-width: 501px) {
+    flex-direction: row;
+    align-items: center;
+  }
 `;

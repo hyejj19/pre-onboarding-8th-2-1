89950f6c0859f02fcde9c431da2ecb05592f9c44
Hyejung Park <hyejj19@naver.com>
2023-01-05 00:46:07 +0900

    📦️ Chore :  Issue dummy data 생성

---
+++ src/DB/Issues.js
@@ -0,0 +1,28 @@
+export const users = ['박혜정', '박산들'];
+
+export const issues = [
+  {
+    id: 1,
+    title: '이슈트래커 퍼블리싱 완성',
+    content: '이슈트래커 퍼블리싱 완성하기',
+    dueDate: '2023-01-04',
+    status: 'todo',
+    personInCharge: 0,
+  },
+  {
+    id: 2,
+    title: '이슈트래커 기능 완성',
+    content: '이슈트래커 기능 완성',
+    dueDate: '2023-01-04',
+    status: 'todo',
+    personInCharge: 1,
+  },
+  {
+    id: 3,
+    title: '로딩 컴포넌트, 딜레이 기능 개발',
+    content: '이슈트래커 기능 완성',
+    dueDate: '2023-01-04',
+    status: 'todo',
+    personInCharge: 0,
+  },
+];

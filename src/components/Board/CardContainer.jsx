b3caa62fe66f2a49af03a21024ce99736389fa9e
Hyejung Park <hyejj19@naver.com>
2023-01-05 15:34:44 +0900

    ✨ Feat :  Issues DnD 1차 구현
    * 같은 status 내에서 순서 변경 가능

--- src/components/Board/CardContainer.jsx
+++ src/components/Board/CardContainer.jsx
@@ -1,18 +1,97 @@
+import { useState } from 'react';
+import { useRecoilState } from 'recoil';
 import styled from 'styled-components';
 
 import Card from '../Card/Card';
 import { getContainerStatus } from './getContainerStatus';
 import { setDelay } from '../../utils/setDelay';
 import { useAddCard } from './hooks/useAddCard';
+import { issuesAtom } from '../../atoms/issuesAtom';
 
-const CardContainer = ({ issues, title }) => {
-  const handleAddCard = useAddCard(title);
+const CardContainer = ({ issues, status }) => {
+  const handleAddCard = useAddCard(status);
+  const [totalIssues, setTotalIssues] = useRecoilState(issuesAtom);
+
+  // Drag & Drop
+  // dragAndDrop 초기값
+  const [dragAndDrop, setDragAndDrop] = useState({
+    draggedFrom: null, // 시작 인덱스
+    draggedTo: null, // 종료 인덱스
+    isDragging: false,
+    originalOrder: [], // 시작 배열 목록
+    updatedOrder: [], // 정렬된 배열 목록
+    updatedIssues: {},
+    status: '',
+  });
+
+  const handleDragStart = (e) => {
+    e.currentTarget.style.opacity = '0.4';
+    const initialPosition = Number(e.target.dataset.position);
+
+    setDragAndDrop({
+      ...dragAndDrop,
+      draggedFrom: initialPosition,
+      originalOrder: totalIssues[status],
+      status: e.target.dataset.status,
+    });
+  };
+
+  const handleDragEnd = (e) => {
+    e.currentTarget.style.opacity = '1';
+  };
+
+  // 다른 item 위에 올라와 있을 때 - 매 ms 마다 발생
+  const handleDragOver = (e) => {
+    e.preventDefault();
+    if (e.target.className.includes('card')) {
+      const { originalOrder, draggedFrom, updatedIssues, status } = dragAndDrop;
+      const draggedTo = Number(e.currentTarget.dataset.position); // 놓을 수 있는 영역의 인덱스(끝)
+      const itemDragged = originalOrder[draggedFrom];
+
+      const newOrder = originalOrder.slice();
+      newOrder.splice(draggedFrom, 1);
+      newOrder.splice(draggedTo, 0, itemDragged);
+
+      if (draggedTo !== dragAndDrop.draggedTo) {
+        // 놓을 수 있는 영역이 변경 되면 객체를 변경해줌
+        setDragAndDrop({
+          ...dragAndDrop,
+          updatedOrder: newOrder,
+          draggedTo,
+          updatedIssues: { ...totalIssues, ...{ [status]: newOrder } },
+        });
+      }
+    }
+  };
+
+  // 다른 item 위로 처음 진입할 때
+  const handleDragEnter = (e) => {
+    // if (e.target.className.includes('card')) console.log(e.target);
+    // if (e.currentTarget.className.includes('cardContainer')) console.log(e.target);
+  };
+
+  const handleDrop = (e) => {
+    setTotalIssues(dragAndDrop.updatedIssues);
+
+    setDragAndDrop({
+      ...dragAndDrop,
+      draggedFrom: null,
+      draggedTo: null,
+    });
+  };
 
   return (
-    <Wrapper>
+    <Wrapper
+      // onDragEnter={(e) => e.target.className.includes('cardContainer') && console.log('drop')}
+      // onDrop={(e) => e.currentTarget.className.includes('cardContainer') && console.log('here')}
+      className={`cardContainer ${status}`}
+      data-status={status}>
       <Menu>
-        <span>{getContainerStatus(title)}</span>
-        <AddBtn onClick={() => setDelay(handleAddCard)}>
+        <span>{getContainerStatus(status)}</span>
+        <AddBtn
+          onClick={() => {
+            setDelay(handleAddCard);
+          }}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -26,8 +105,17 @@ const CardContainer = ({ issues, title }) => {
       </Menu>
 
       <CardWrapper>
-        {issues.map((issue) => (
-          <Card key={issue.id} issue={issue} />
+        {issues.map((issue, idx) => (
+          <Card
+            key={issue.id}
+            issue={issue}
+            idx={idx}
+            handleDragStart={handleDragStart}
+            handleDragEnd={handleDragEnd}
+            handleDragOver={handleDragOver}
+            handleDragEnter={handleDragEnter}
+            handleDrop={handleDrop}
+          />
         ))}
       </CardWrapper>
     </Wrapper>
@@ -39,6 +127,7 @@ export default CardContainer;
 const Wrapper = styled.div`
   width: 100%;
   height: 100%;
+  min-height: 100px;
   padding: 0.5rem;
 `;
 
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,12 +1,84 @@
 import styled from 'styled-components';
 
-import { users } from '../../DB/Issues';
+import { useEffect, useRef, useState } from 'react';
+import { useRecoilState } from 'recoil';
+import { issuesAtom } from '../../atoms/issuesAtom';
+import { users } from '../../mock/Issues';
+import { useDeleteCard } from './hooks/useDeleteCard';
+import { setDelay } from '../../utils/setDelay';
+import { issuesAPI } from '../../api/issues';
 
-const Card = ({ issue }) => {
+const Card = ({ issue, handleDragStart, handleDragEnd, handleDragOver, handleDragEnter, handleDrop, idx }) => {
+  const handleDeleteCard = useDeleteCard(issue, issue.id);
+  const [issues, setIssues] = useRecoilState(issuesAtom);
+
+  const [isEditMode, setIsEditMode] = useState({
+    title: false,
+    personInCharge: false,
+    dueDate: false,
+  });
+
+  const titleRef = useRef(null);
+  const cardRef = useRef(null);
+
+  const handleEditMode = (subject) => {
+    setIsEditMode({
+      ...isEditMode,
+      ...{ [subject]: !isEditMode[subject] },
+    });
+    issuesAPI.setIssues(issues);
+  };
+
+  useEffect(() => {
+    if (isEditMode.title) titleRef.current.focus();
+  }, [isEditMode]);
+
+  const [titleValue, setTitleValue] = useState(issue.title);
+
+  const handleTitleValue = (e) => {
+    setTitleValue(e.target.value);
+  };
+
   return (
-    <Wrapper>
-      {/* <IssueTitle type="text" value={issue.title} /> */}
-      <span>{issue.title}</span>
+    <Wrapper
+      draggable
+      onDragStart={handleDragStart}
+      onDragEnd={handleDragEnd}
+      onDragOver={handleDragOver}
+      onDragEnter={handleDragEnter}
+      onDrop={handleDrop}
+      ref={cardRef}
+      className={`card ${issue.status}`}
+      data-position={idx}
+      data-status={issue.status}>
+      {isEditMode.title ? (
+        <IssueTitle
+          type="text"
+          value={titleValue}
+          onBlur={() => handleEditMode('title')}
+          onKeyDown={(e) => e.key === 'Enter' && handleEditMode('title')}
+          ref={titleRef}
+          onChange={handleTitleValue}
+        />
+      ) : (
+        <div onClick={() => handleEditMode('title')}>{titleValue}</div>
+      )}
+
+      <DeleteBtn onClick={() => setDelay(handleDeleteCard)}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="w-6 h-6">
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+          />
+        </svg>
+      </DeleteBtn>
       <span>{users[issue.personInCharge]}</span>
       <span>{issue.dueDate}</span>
     </Wrapper>
@@ -14,6 +86,22 @@ const Card = ({ issue }) => {
 };
 
 export default Card;
+
+const DeleteBtn = styled.div`
+  width: 1.2rem;
+  height: 1.2rem;
+  cursor: pointer;
+  position: absolute;
+  right: 12px;
+  visibility: hidden;
+
+  :hover {
+    > svg {
+      stroke: var(--hover-color);
+      transition: all linear 0.1s;
+    }
+  }
+`;
 
 const Wrapper = styled.div`
   width: 100%;
@@ -23,10 +111,24 @@ const Wrapper = styled.div`
   margin-bottom: 10px;
   border-radius: 10px;
   box-shadow: rgba(99, 99, 103, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.046) 0px 1px 3px -1px;
+  position: relative;
 
   > * {
     margin-bottom: 0.7rem;
   }
+
+  .over {
+    background-color: red;
+  }
+
+  :hover {
+    ${DeleteBtn} {
+      transition: all linear 0.1s;
+      visibility: visible;
+    }
+  }
 `;
 
-const IssueTitle = styled.input``;
+const IssueTitle = styled.input`
+  margin-right: 15px;
+`;
